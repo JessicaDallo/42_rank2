7008619e9ab86743f85a6f9cfd2e6f5074ea73eb
Jessica Silva De Oliveira Dallo <jesilva-@c1r1s2.42porto.com>
2024-05-31 09:40:58 +0100

    final add

---
+++ minitalk/minitalk.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jesilva- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/27 11:30:01 by jesilva-          #+#    #+#             */
+/*   Updated: 2024/05/27 11:33:34 by jesilva-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "./ft_printf/ft_printf.h"
+# include <signal.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <stddef.h>
+# include <stdlib.h>
+
+#endif
